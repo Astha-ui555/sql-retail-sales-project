732326fe0b610c4e8fe13c2d3868a74b3fcac3ee
Astha-ui555 <asthapandey6394@gmail.com>
2025-10-06 06:02:39 -0700

    Update my sql proje

--- my sql project #.sql
+++ my sql project #.sql
@@ -23,13 +23,8 @@ LIMIT 10;
 
 SELECT COUNT(*) FROM retail_sales ;
 
-SELECT*FROM retail_sales 
-WHERE transactions_id IS NULL
 
-SELECT*FROM retail_sales 
-WHERE sale_date IS NULL
-
-
+--Data Finding (Null Values)
 SELECT*FROM retail_sales 
 WHERE 
     transactions_id IS NULL
@@ -53,6 +48,7 @@ WHERE
       cogs IS NULL
 	 OR
      total_sale IS NULL ;
+
 
 -- Data Cleaning
 DELETE FROM retail_sales
@@ -79,7 +75,7 @@ WHERE
 	 OR
      total_sale IS NULL ;
 
-  -- Data Exploration
+  -- Data Explorations
 
   --How many sales we have?
   SELECT COUNT (*) as total_sales FROM retail_sales;
@@ -93,19 +89,16 @@ WHERE
   -- Data Analysis & Business Key Problems & Answers.
 
   -- My Analysis & Findings
-
-  
 -- Q1. Write a SQL query to retrieve all columns for the sale made on '2022-11-05'.
 -- Q2.Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022.
--- 3.Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.:
--- 4.Write a SQL query to calculate the total sales (total_sale) for each category.
--- 5.Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022:
---6.Write a SQL query to calculate the average sale for each month. Find out best selling month in each year:
--- 7.Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.:
--- 8.Write a SQL query to find all transactions where the total_sale is greater than 1000.:
--- 9.*Write a SQL query to find the top 5 customers based on the highest total sales *:
--- 10.Write a SQL query to find the number of unique customers who purchased items from each category.
--- 11.Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17):
+-- Q3.Write a SQL query to calculate the total sales (total_sale) for each category.
+-- Q4.Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+-- Q5..Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q6.Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+-- Q7.Write a SQL query to calculate the average sale for each month. Find out best selling month in each year.
+-- Q8.*Write a SQL query to find the top 5 customers based on the highest total sales *.
+-- Q9..Write a SQL query to find the number of unique customers who purchased items from each category.
+-- Q10.Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17).
 
 
 -- Q1. Write a SQL query to retrieve all columns for the sale made on '2022-11-05'.
@@ -220,6 +213,7 @@ GROUP BY shift
 
 
 -- END OF PROJECT !!!
+
 
 
 
